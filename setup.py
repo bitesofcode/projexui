9d2347a5651a4e35b77df493cb2d069a34369f5c
Eric Hulser <ehulser@teslamotors.com>
2015-03-09 09:47:14 -0700

    migrated package to the root directory

---
+++ setup.py
@@ -0,0 +1,24 @@
+import projexui
+from setuptools import setup, find_packages
+
+setup(
+    name = 'projexui',
+    version = projexui.__version__,
+    author = 'Eric Hulser',
+    author_email = 'eric.hulser@gmail.com',
+    maintainer = 'Eric Hulser',
+    maintainer_email = 'eric.hulser@gmail.com',
+    description = 'Library of Qt extension widgets.',
+    license = 'LGPL',
+    keywords = '',
+    url = 'https://github.com/ProjexSoftware/projexui',
+    include_package_data=True,
+    packages = find_packages(),
+    install_requires = [
+        'projex',
+        'xqt'
+    ],
+    tests_require = [],
+    long_description= 'Library of Qt extension widgets.',
+    classifiers=[],
+)
